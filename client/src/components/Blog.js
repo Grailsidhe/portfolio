3797887762274db9de22be16b02e3699cc8d1c68
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-11-14 17:07:12 +0100

    Added blog

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -5,30 +5,46 @@ import "./css/Blog.css";
 
 export default function Blog() {
 
-    const [data1, setData1] = useState();
+    const [data, setData] = useState();
+    const [keywLinks, setKeywLinks] = useState([]);
 
     useEffect(() => {
         axios
             .get(`/api/blogposts`)
-            .then((response) => setData1(response.data))
+            .then((response) => setData(response.data.reverse()))
             .catch((error) => {
                 console.log(error);
             });
     }, []);
+
+    useEffect(() => {
+        const tempKeywords = []
+        data && data.forEach((item)=> {
+            const noRepeat = item.keywords.split(",");
+            tempKeywords.push(noRepeat);
+            setKeywLinks([...tempKeywords]);
+        })
+    }, [data]);
+
+    console.log(keywLinks)
+    console.log(1, data)
     
     return (
         <div className="Blog-wrapper">
-            Blog Blog
-            {data1 && data1.map((item, i)=> {
-                <BlogPost 
-                    key={i}
-                    title={item.title}
-                    date={item.date}
-                    postbody={item.postbody}
-                    picture={item.picture}
-                    keywords={item.keywords}
-                />
-            })}
+            Blog
+            <div className="Blog-keywords">{keywLinks}</div>
+            {data && data.map((item, i)=> {
+                return ( 
+                    <BlogPost 
+                        key={i}
+                        title={item.title}
+                        date={item.date}
+                        postbody={item.postbody}
+                        picture={item.picture}
+                        keywords={item.keywords}
+                    />
+                )}
+            )}
         </div>
     )
 };
--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -1,21 +1,36 @@
-import React, { useState, useContext } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useContext, useEffect } from 'react';
+// import { Link } from "react-router-dom";
 import "./css/Blog.css";
-import Context from './Context';
+// import Context from './Context';
 
 export default function BlogPost({key, title, postbody, date, picture, keywords}) {
     
-    const { mobile, setMobile, active, setActive } = useContext(Context);
+    // const { mobile, setMobile, active, setActive } = useContext(Context);
+    const [eachKey, setEachKey] = useState();
+
+    useEffect(() => { setEachKey(keywords.split(",")) })
 
     return (
         <div className="BlogPost-wrapper">
             <div className="BlogPost-post" key={key}>
-                <div>
-                    {title}{date}
-                    <div>
-                        {postbody}
-                    </div>
-                    {keywords}
+                <div className="BlogPost-title-wrap">
+                    <span className="BlogPost-title">{title}</span>
+                    <span className="BlogPost-date">{date}</span>
+                </div>
+                <div className="BlogPost-body-wrap">
+                    <span className="BlogPost-picture">
+                        {picture && <img src={picture} alt="Post image" />}
+                    </span>
+                    <span className="BlogPost-text">{postbody}</span>
+                </div>
+                <div className="BlogPost-keyword-wrap">
+                    {eachKey && eachKey.map((item)=>{
+                        return(
+                        <span className="BlogPost-keyword">
+                            <a href="">{item}</a>
+                        </span>
+                        )
+                    })}
                 </div>
             </div>
         </div>
